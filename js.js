01e0a9c9ee2f81c437a9759d82f494c2aded4afe
Ballademager <118718923+Ballademager@users.noreply.github.com>
2023-08-31 23:25:30 +0200

    yoyo

--- js.js
+++ js.js
@@ -13,8 +13,23 @@ const texts = {
     ],
   },
 };
+
+console.log("original object", texts);
+const english = {
+  en: {
+    texts: [
+      { text: "The Boat", location: ".header" },
+      { text: "The Robot", location: ".footer" },
+    ],
+  },
+};
+
+Object.assign(texts, english);
+console.log("added a lang. ", texts);
+
 const local = "da";
 const local_de = "de";
+const local_en = "en";
 const selection = document.querySelector("#language");
 
 console.log(texts.da.texts[0]);
@@ -23,14 +38,27 @@ console.log(texts.da.texts[0]);
 selection.addEventListener("change", () => {
   if (selection.value === "dan") {
     order66(local);
-  } else {
+  } else if (selection.value === "deu") {
     order66(local_de);
+  } else if (selection.value === "eng") {
+    order66(local_en);
   }
 });
 
 order66(local);
 function order66(sprog) {
   console.log("change happened");
+  document.querySelector(".danish-flag").classList.add("hidden");
+  document.querySelector(".german-flag").classList.add("hidden");
+  document.querySelector(".english-flag").classList.add("hidden");
   document.querySelector(".header").textContent = texts[sprog].texts[0].text;
   document.querySelector(".footer").textContent = texts[sprog].texts[1].text;
+  console.log(sprog);
+  if (sprog === "da") {
+    document.querySelector(".danish-flag").classList.remove("hidden");
+  } else if (sprog === "de") {
+    document.querySelector(".german-flag").classList.remove("hidden");
+  } else if (sprog === "en") {
+    document.querySelector(".english-flag").classList.remove("hidden");
+  }
 }
